b9c62b8570328505a714f5f41515a90b3c56bb67
moer <moer@cubee.com>
2019-04-23 18:54:37 +0800

    修复错误

--- server/routers/index.ts
+++ server/routers/index.ts
@@ -1,7 +1,7 @@
-import * as combineRouters from 'koa-combine-routers'
-import * as webRouters from './web'
-import * as apiRouters from './api'
-
-const router = combineRouters(webRouters, apiRouters)
-
-export default router
+// import * as combineRouters from 'koa-combine-routers'
+// import * as webRouters from './web'
+// import * as apiRouters from './api'
+//
+// const router = combineRouters(webRouters, apiRouters)
+//
+// export default router
